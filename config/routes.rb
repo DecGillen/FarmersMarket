e92afd03bb27207126574ac197ae4888ffe14641
D Gillen <declangillen@live.com>
2023-08-10 11:44:46 +0100

    fixed paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
 
   get 'buy', to: 'products#buy'
+  get 'read', to: 'products#read'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
